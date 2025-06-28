9fdce0c523f54c4958fdbd02bfbb973506db8da4
Sebastian Iturralde <itusebastian@gmail.com>
2025-06-28 15:44:18 -0500

    Agregar clase Temporada y relacionarla con SerieDeTV

--- src/uni1a/SerieDeTV.java
+++ src/uni1a/SerieDeTV.java
@@ -3,31 +3,37 @@
  */
 package uni1a;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // Subclase SerieDeTV que extiende de ContenidoAudiovisual
 public class SerieDeTV extends ContenidoAudiovisual {
-    private int temporadas;
+    private List<Temporada> temporadas;
 
-    public SerieDeTV(String titulo, int duracionEnMinutos, String genero, int temporadas) {
+    public SerieDeTV(String titulo, int duracionEnMinutos, String genero) {
         super(titulo, duracionEnMinutos, genero);
-        this.temporadas = temporadas;
+        this.temporadas = new ArrayList<>();
     }
 
-    public int getTemporadas() {
+    public void agregarTemporada(Temporada temporada) {
+        this.temporadas.add(temporada);
+    }
+
+    public List<Temporada> getTemporadas() {
         return temporadas;
     }
 
-    public void setTemporadas(int temporadas) {
-        this.temporadas = temporadas;
-    }
-    
     @Override
     public void mostrarDetalles() {
-        System.out.println("Detalles de la película:");
+        System.out.println("Detalles de la serie de TV:");
         System.out.println("ID: " + getId());
         System.out.println("Título: " + getTitulo());
         System.out.println("Duración en minutos: " + getDuracionEnMinutos());
         System.out.println("Género: " + getGenero());
-        System.out.println("Temporadas: " + this.temporadas);
+        System.out.println("Temporadas: " + temporadas.size());
+        for (Temporada t : temporadas) {
+            t.mostrarDetalles();
+        }
         System.out.println();
     }
 }
---
+++ src/uni1a/Temporada.java
@@ -0,0 +1,31 @@
+package uni1a;
+
+public class Temporada {
+    private int numero;
+    private int cantidadEpisodios;
+
+    public Temporada(int numero, int cantidadEpisodios) {
+        this.numero = numero;
+        this.cantidadEpisodios = cantidadEpisodios;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getCantidadEpisodios() {
+        return cantidadEpisodios;
+    }
+
+    public void setCantidadEpisodios(int cantidadEpisodios) {
+        this.cantidadEpisodios = cantidadEpisodios;
+    }
+
+    public void mostrarDetalles() {
+        System.out.println("Temporada " + numero + ": " + cantidadEpisodios + " episodios");
+    }
+}
