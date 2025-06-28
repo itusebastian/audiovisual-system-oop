35214133cf1f88edb090b437fbb7ff046b93305f
Sebastian Iturralde <itusebastian@gmail.com>
2025-06-28 16:01:06 -0500

    Actualizar instanciación de subclases para reflejar nuevas relaciones

--- src/poo/PruebaAudioVisual.java
+++ src/poo/PruebaAudioVisual.java
@@ -5,11 +5,20 @@ public class PruebaAudioVisual {
 	public static void main(String[] args) {
         System.out.println("Hello from Eclipse!");
 
-        // Crear instancias de las subclases
-        ContenidoAudiovisual[] contenidos = new ContenidoAudiovisual[3];
-        contenidos[0] = new Pelicula("Avatar", 125, "Accion", "20th Century Studios");
-        contenidos[1] = new SerieDeTV("Game of Thrones", 60, "Fantasy", 8);
-        contenidos[2] = new Documental("Cosmos", 45, "Science", "Astronomy");
+        // Crear instancias de las subclases actualizadas
+        Pelicula pelicula = new Pelicula("Avatar", 125, "Accion", "20th Century Studios");
+        pelicula.agregarActor(new Actor("Sam Worthington", 45, "Australia"));
+        pelicula.agregarActor(new Actor("Zoe Saldana", 44, "Estados Unidos"));
+
+        SerieDeTV serie = new SerieDeTV("Game of Thrones", 60, "Fantasy");
+        serie.agregarTemporada(new Temporada(1, 10));
+        serie.agregarTemporada(new Temporada(2, 10));
+
+        Documental documental = new Documental("Cosmos", 45, "Science", "Astronomy");
+        documental.agregarInvestigador(new Investigador("Carl Sagan", "Astronomía"));
+        documental.agregarInvestigador(new Investigador("Ann Druyan", "Guionismo"));
+
+        ContenidoAudiovisual[] contenidos = new ContenidoAudiovisual[] {pelicula, serie, documental};
 
         // Mostrar los detalles de cada contenido audiovisual
         for (ContenidoAudiovisual contenido : contenidos) {
