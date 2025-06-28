b21acfc32ccd36f5485559281bb84a9eaf626430
Sebastian Iturralde <itusebastian@gmail.com>
2025-06-28 15:49:06 -0500

    Agregar clase Investigador y asociarla a Documental

--- src/uni1a/Documental.java
+++ src/uni1a/Documental.java
@@ -3,13 +3,26 @@
  */
 package uni1a;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // Subclase Documental que extiende de ContenidoAudiovisual
 public class Documental extends ContenidoAudiovisual {
     private String tema;
+    private List<Investigador> investigadores;
 
     public Documental(String titulo, int duracionEnMinutos, String genero, String tema) {
         super(titulo, duracionEnMinutos, genero);
         this.tema = tema;
+        this.investigadores = new ArrayList<>();
+    }
+
+    public void agregarInvestigador(Investigador investigador) {
+        this.investigadores.add(investigador);
+    }
+
+    public List<Investigador> getInvestigadores() {
+        return investigadores;
     }
 
     public String getTema() {
@@ -28,6 +41,10 @@ public class Documental extends ContenidoAudiovisual {
         System.out.println("Duración en minutos: " + getDuracionEnMinutos());
         System.out.println("Género: " + getGenero());
         System.out.println("Tema: " + this.tema);
+        System.out.println("Investigadores: " + investigadores.size());
+        for (Investigador inv : investigadores) {
+            inv.mostrarDetalles();
+        }
         System.out.println();
     }
 }
