33db4a7124e9acdb6c7667473ae6b6e3697da4b5
Sebastian Iturralde <itusebastian@gmail.com>
2025-06-28 15:42:23 -0500

    Agregar clase Actor y relacionarla con Pelicula

--- src/uni1a/Pelicula.java
+++ src/uni1a/Pelicula.java
@@ -3,13 +3,18 @@
  */
 package uni1a;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // Subclase Pelicula que extiende de ContenidoAudiovisual
 public class Pelicula extends ContenidoAudiovisual {
     private String estudio;
+    private List<Actor> actores;
 
     public Pelicula(String titulo, int duracionEnMinutos, String genero, String estudio) {
         super(titulo, duracionEnMinutos, genero);
         this.estudio = estudio;
+        this.actores = new ArrayList<>();
     }
 
     public String getEstudio() {
@@ -18,6 +23,25 @@ public class Pelicula extends ContenidoAudiovisual {
 
     public void setEstudio(String estudio) {
         this.estudio = estudio;
+    }
+
+    public List<Actor> getActores() {
+        return actores;
+    }
+
+    public void agregarActor(Actor actor) {
+        this.actores.add(actor);
+    }
+
+    public void mostrarActores() {
+        if (actores.isEmpty()) {
+            System.out.println("No hay actores registrados para esta película.");
+        } else {
+            System.out.println("Actores:");
+            for (Actor actor : actores) {
+                actor.mostrarDetalles();
+            }
+        }
     }
     
     @Override
@@ -28,6 +52,7 @@ public class Pelicula extends ContenidoAudiovisual {
         System.out.println("Duración en minutos: " + getDuracionEnMinutos());
         System.out.println("Género: " + getGenero());
         System.out.println("Estudio: " + estudio);
+        mostrarActores();
         System.out.println();
     }
 }
